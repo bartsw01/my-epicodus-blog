309b8e107a81b44951e61e824dfb5dab768a9a88
Bart Webber, Maria Thomas and Paul Guevarra <maria@>
2017-08-07 14:28:55 -0700

    add 2 tests - passed

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * Created by Guest on 8/7/17.
+ */
+public class Post {
+    private String content;
+
+    public Post (String content){
+        this.content = content;g
+
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,25 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/7/17.
+ */
+public class PostTest {
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
+    }
+
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+    }
+}
